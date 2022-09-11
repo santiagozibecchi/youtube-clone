2170bee3746230cf0cd2d706db020312e17a5206
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-11 04:11:29 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import VideoDetail from "./components/VideoDetail";
+import ChanelDetail from "./components/ChanelDetail";
+import SearchFeed from "./components/SearchFeed";
+import Feed from "./components/Feed";
 
-const App = () => {
-   return (
-      <div>
-         <h1>Hello world</h1>
-      </div>
-   );
-};
+const App = () => (
+   <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+         <Navbar />
+         <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/channel/:id" element={<ChanelDetail />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+         </Routes>
+      </Box>
+   </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/ChanelDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChanelDetail = () => {
+   return <div>ChanelDetail</div>;
+};
+
+export default ChanelDetail;
