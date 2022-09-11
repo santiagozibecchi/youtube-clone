7736d1c3f74f2899fa68c9286a42c925eeb815f9
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-11 04:37:31 -0300

    SearchBar

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as Feed } from "./Feed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as ChanelDetail } from "./ChanelDetail";
+export { default as SearchBar } from "./SearchBar";
