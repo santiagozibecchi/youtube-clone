44a9db926155e53d42065916c8581d1c5d978d65
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-12 18:39:05 -0300

    pasando estilo opcionales por props

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,13 +3,14 @@ import { Box, Stack } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
-   console.log(videos);
+   // console.log(videos);
    return (
       <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
          {videos.map((item, index) => (
             <Box key={index}>
                {item.id.videoId && <VideoCard video={item} />}
-               {item.id.channelId && <ChannelCard ChannelDetail={item} />}
+               {/* {console.log(item.id)} */}
+               {item.id.channelId && <ChannelCard channelDetail={item} />}
             </Box>
          ))}
       </Stack>
